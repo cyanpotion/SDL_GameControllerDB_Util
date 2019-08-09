05de7ecdd2cef686964b59aa603ca21109918571
XenoAmess <xenoamess@gmail.com>
2019-08-09 17:34:13 +0800

    delete unused codes. refine test.

--- src/main/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_Util.java
+++ src/main/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_Util.java
@@ -4,12 +4,7 @@ import java.io.*;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * @author XenoAmess
@@ -53,10 +48,15 @@ public class SDL_GameControllerDB_Util {
      *
      * @return gamecontrollerdb.txt's content and generate a temp file with it.
      */
-    public static File getSDL_GameControllerDB_TempFile() throws IOException, IllegalStateException {
-        File res = Files.createTempFile(null, null).toAbsolutePath().toFile();
-        try (FileWriter fileWriter = new FileWriter(res)) {
-            fileWriter.write(getSDL_GameControllerDB_String());
+    public static File getSDL_GameControllerDB_TempFile() {
+        File res = null;
+        try {
+            Files.createTempFile(null, null).toAbsolutePath().toFile();
+            try (FileWriter fileWriter = new FileWriter(res)) {
+                fileWriter.write(getSDL_GameControllerDB_String());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return res;
     }
@@ -65,12 +65,6 @@ public class SDL_GameControllerDB_Util {
         final URL res = SDL_GameControllerDB_Util.class.getResource(resourceFilePath);
         return res;
     }
-
-    private static File getFile(String resourceFilePath) {
-        final URL resUrl = getURL(resourceFilePath);
-        return new File(resUrl.getFile().replaceAll("%20", " "));
-    }
-
 
     private static String loadFile(String resourceFilePath) {
         String res = "";
@@ -93,64 +87,6 @@ public class SDL_GameControllerDB_Util {
             e.printStackTrace();
         }
         return res;
-    }
-
-    /**
-     * Reads the specified resource and returns the raw data as a ByteBuffer.
-     *
-     * @param resourceFile the resource file to read
-     * @return the resource data
-     */
-    public static ByteBuffer loadFileBuffer(File resourceFile) {
-        boolean success;
-
-        ByteBuffer buffer = null;
-        final String absolutePath = resourceFile.getAbsolutePath();
-        Path path = Paths.get(absolutePath);
-        if (Files.isReadable(path)) {
-            try (SeekableByteChannel fc = Files.newByteChannel(path)) {
-                buffer = createByteBuffer((int) fc.size() + 1);
-                while (fc.read(buffer) != -1) {
-                }
-                success = true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                if (buffer != null) {
-                    buffer.clear();
-                }
-                success = false;
-            }
-            if (success) {
-                buffer.flip();
-                return buffer.slice();
-            }
-        }
-
-
-        try (
-                InputStream source = new FileInputStream(resourceFile);
-                ReadableByteChannel rbc = Channels.newChannel(source)
-        ) {
-            buffer = createByteBuffer((int) resourceFile.length() + 1);
-
-            while (true) {
-                int bytes = rbc.read(buffer);
-                if (bytes == -1) {
-                    break;
-                }
-                //                //                    if (buffer.remaining() == 0) {
-                //                //                        buffer = resizeBuffer(buffer, buffer.capacity() * 3 / 2);
-                // 50%
-                //                //                    }
-            }
-            success = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            success = false;
-        }
-        assert (success);
-        buffer.flip();
-        return buffer.slice();
     }
 
     /**
