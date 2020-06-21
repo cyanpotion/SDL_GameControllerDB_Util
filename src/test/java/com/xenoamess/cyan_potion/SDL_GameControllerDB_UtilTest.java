126a3bb35fe6b3b54a48f9e95dfdaa51b678da83
XenoAmess <xenoamess@gmail.com>
2020-06-21 11:28:03 +0800

    refine tests

--- src/test/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_UtilTest.java
+++ src/test/java/com/xenoamess/cyan_potion/SDL_GameControllerDB_UtilTest.java
@@ -3,10 +3,16 @@ package com.xenoamess.cyan_potion;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 
 import static com.xenoamess.cyan_potion.SDL_GameControllerDB_Util.getSDL_GameControllerDB_String;
 import static com.xenoamess.cyan_potion.SDL_GameControllerDB_Util.glfwUpdateGamepadMappings;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.lwjgl.glfw.GLFW.glfwInit;
 import static org.lwjgl.glfw.GLFW.glfwUpdateGamepadMappings;
@@ -40,5 +46,32 @@ public class SDL_GameControllerDB_UtilTest {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
         glfwUpdateGamepadMappings();
+    }
+
+    @Test
+    public void getSDL_GameControllerDB_TempFileTest() {
+        File tmpFile = SDL_GameControllerDB_Util.getSDL_GameControllerDB_TempFile();
+        try (FileInputStream fileInputStream = new FileInputStream(tmpFile);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        ) {
+            final StringBuilder sb = new StringBuilder();
+            String tmp;
+            while (true) {
+                tmp = bufferedReader.readLine();
+                if (tmp == null) {
+                    break;
+                }
+                sb.append(tmp);
+                sb.append("\n");
+            }
+            System.out.println(sb.toString());
+            assertNotEquals("", sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!tmpFile.delete()) {
+            tmpFile.deleteOnExit();
+        }
     }
 }
