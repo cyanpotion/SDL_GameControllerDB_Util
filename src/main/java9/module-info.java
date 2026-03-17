8dc6a26b554932076d994a0cb242dc85d2ecc767
XenoAmess <xenoamess@gmail.com>
2026-03-17 17:08:46 +0000

    fix: remove LWJGL module dependencies from module-info.java
    LWJGL 3.4.1 has strict module requirements that break compilation.
    Since LWJGL is an optional/provided dependency, remove the
    requires statements from module-info.java to fix the build.
    
    Fixes PR #218 build failure.

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,6 +1,4 @@
 module com.xenoamess.cyan_potion.sdl_game_controller_db_util {
-    requires static org.lwjgl;
-    requires static org.lwjgl.glfw;
     exports com.xenoamess.cyan_potion;
     exports com.xenoamess.cyan_potion.sdl_game_controller_db_util;
 }
